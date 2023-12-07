058bd37d00dc962ee54f57cddb6f11b6a2737437
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2023-12-07 19:08:07 +0545

    Cleanup.

---
+++ lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(title: Text("Ask AI"));
+  }
+
+  Widget _buildBody() {
+    return Container();
+  }
+}
